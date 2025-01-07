a0a1d521233426f55b0786f8e851ebc94f6bbc9e
Jenna Miles Reyes <atienzajennamiles@gmail.com>
2025-01-08 01:11:59 +0800

    Navigation for the user side pages

--- home.php
+++ home.php
@@ -4,17 +4,55 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PesoBuddy|Home</title>
+    <title>PesoBuddy | Home</title>
+    <link rel="icon" href="assets/images/pesobuddy_icon.png" />
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Exa&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <!-- START HERE -->
-    <div>
+    <div class="p-1 mb-3 border-bottom bg-gray">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <!-- Logo -->
+                <a class="navbar-brand" href="index.php" style="margin-left: -25px;">
+                    <img src="assets/images/pesobuddy_logo.png" width="230" height="50" alt="Logo">
+                </a>
 
+                <!-- Toggler Button -->
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <!-- Collapsible Menu -->
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link px-2 link-body-emphasis subheading" href="#">Overview</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link px-2 link-body-emphasis subheading" href="#">Tracker</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link px-2 link-body-emphasis subheading" href="#">Transaction</a>
+                        </li>
+                    </ul>
+
+                    <!-- User Profile Image -->
+                    <div class="px-2 ms-lg-3">
+                        <a href="#">
+                            <img src="assets/images/pesobuddy_icon.png" alt="mdo" width="32" height="32"
+                                class="rounded-circle">
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </div>
     </div>
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
