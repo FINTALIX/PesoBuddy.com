b17273506279602124b7c2297a03b372fb74d763
chinrstyabclno <153152303+chinrstyabclno@users.noreply.github.com>
2025-01-09 16:40:14 +0800

    FE - Added footer.php and footer.css

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer class="fixed-bottom">
+        <div class="footer-left">
+            <p>CONTACT US</p>
+        </div>
+        <div class="social-icons">
+            <a href="https://facebook.com" target="_blank"><img src="assets/images/facebook.png" alt="Facebook"></a>
+            <a href="https://instagram.com" target="_blank"><img src="assets/images/instagram.png" alt="Instagram"></a>
+            <a href="https://twitter.com" target="_blank"><img src="assets/images/twitter.png" alt="Twitter"></a>
+            <a href="https://github.com" target="_blank"><img src="assets/images/github.png" alt="GitHub"></a>
+            <img src="assets/images/fintalix-logo.webp" alt="Fintalix Logo">
+        </div>
+        <div class="footer-right">
+            <p>FINTALIX &copy; 2025</p>
+        </div>
+</footer>`
