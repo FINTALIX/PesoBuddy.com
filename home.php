d0f3f8ec7cc87557bbe8021dc9abd8e58ef24273
Jenna Miles Reyes <atienzajennamiles@gmail.com>
2025-01-09 20:41:04 +0800

    Remove body background color

--- home.php
+++ home.php
@@ -7,7 +7,6 @@
     <title>PesoBuddy</title>
     <link rel="icon" href="assets/images/pesobuddy_icon.png" />
     <link rel="stylesheet" href="assets/css/style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Lexend+Exa&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
