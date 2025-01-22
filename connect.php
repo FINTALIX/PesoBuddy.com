6fa0b53db0790f574c9daf745e7fd56eaa62b460
AJBernardo <austinebernardo8104@gmail.com>
2025-01-22 23:18:22 +0800

    [BE] - Implementing Functionality to the Back-End of the User Dashboard Budget Tracker Section

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
   $dbhost = "localhost";
 	$dbuser = "root";
 	$dbpass = "";
-	$db = "pesobuddy2";
+	$db = "pesobuddy";
 
 	$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
