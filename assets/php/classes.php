2268f52f2492d77d9120184e4ab9441c123ef8b9
AJBernardo <austinebernardo8104@gmail.com>
2025-01-23 18:56:18 +0800

    [BE] - Implementing Functionality to the Back-End of the Manage Users

--- assets/php/classes.php
+++ assets/php/classes.php
@@ -216,11 +216,11 @@ class TransactionsHistory
             '
             <tr>
                 <td scope="row" class="text-center">' . $transactionNo . '</td>
-                <td>'.$this->transactionType.'</td>
-                <td>'.$this->transactionCategory.'</td>
-                <td>'.number_format($this->transactionAmount, 2).'</td>
-                <td>'.date('F j, Y', strtotime($this->transactionDate)).'</td>
-                <td>'.$this->transactionDescription.'</td>
+                <td>' . $this->transactionType . '</td>
+                <td>' . $this->transactionCategory . '</td>
+                <td>' . number_format($this->transactionAmount, 2) . '</td>
+                <td>' . date('F j, Y', strtotime($this->transactionDate)) . '</td>
+                <td>' . $this->transactionDescription . '</td>
 
                 <td>
                     <div class="dropdown dropstart">
@@ -233,7 +233,7 @@ class TransactionsHistory
                             <!-- Edit Button -->
                             <li>
                                 <a class="dropdown-item option-dropdown" data-bs-toggle="modal"
-                                    data-bs-target="#editTransaction'.$this->transactionID.'"
+                                    data-bs-target="#editTransaction' . $this->transactionID . '"
                                     style="text-decoration: none;">
                                     <i class="bi bi-pencil-square px-1"></i> Edit
                                 </a>
@@ -242,7 +242,7 @@ class TransactionsHistory
                             <!-- Delete Button -->
                             <li>
                                 <a class="dropdown-item option-dropdown" data-bs-toggle="modal"
-                                    data-bs-target="#deleteTransaction'.$this->transactionID.'"
+                                    data-bs-target="#deleteTransaction' . $this->transactionID . '"
                                     style="color: red; text-decoration: none;">
                                     <i class="bi bi-trash3 px-1"></i> Delete
                                 </a>
@@ -256,11 +256,11 @@ class TransactionsHistory
 
     public function editTransaction()
     {
-        if(isset($_POST['btnEditTransaction'])) {
+        if (isset($_POST['btnEditTransaction'])) {
             // Set Category
             $categoryInput = explode("_", $_POST['transactionCategory']);
 
-            $categoryKind = $categoryInput[0]; 
+            $categoryKind = $categoryInput[0];
             $categoryID = $categoryInput[1];
 
             $defaultCategoryID = null;
@@ -288,7 +288,7 @@ class TransactionsHistory
 
     public function deleteTransaction()
     {
-        if(isset($_POST['btnDeleteTransaction'])) {
+        if (isset($_POST['btnDeleteTransaction'])) {
             $transactionID = $_POST['transactionID'];
 
             $deleteTransactionQuery = "DELETE FROM transactions WHERE transactionID = '$transactionID' AND userID = '{$_SESSION['userID']}'";
@@ -629,8 +629,9 @@ if (isset($_POST['deleteCategory'])) {
     exit;
 }
 
-class User {
-    
+class User
+{
+
     public $userResult;
     public $userID;
     public $firstName;
@@ -642,19 +643,46 @@ class User {
     public $status;
     public $profilePicture;
 
-    public function queryAllUsers() {
+    public function queryAllUsers()
+    {
         $userQuery = "SELECT users.*, logins.loginDate AS recentLogin FROM users
         LEFT JOIN logins ON users.userID = logins.userID
         WHERE users.role = 'user' AND logins.loginDate = (
             SELECT MAX(loginDate) 
             FROM logins 
-            WHERE logins.userID = users.userID)
-        ORDER BY users.userID";
+            WHERE logins.userID = users.userID)";
+
+        if (isset($_GET['searchUser']) && (!empty($_GET['searchUser']))) {
+            $searchUser = $_GET['searchUser'];
+
+            if (strcasecmp($searchUser, 'Clear') !== 0) // insert this
+                $userQuery .= " AND ((username LIKE '%$searchUser%') OR (firstName LIKE '%$searchUser%') OR (lastName LIKE '%$searchUser%'))";
+            else {
+                header("Location: users.php");
+                exit();
+            }
+
+        }
+
+        $userQuery .= " ORDER BY ";
+
+        if (isset($_GET['sortBy']) && (!empty($_GET['sortBy']))) {
+            $sortOption = $_GET['sortBy'];
+            $userQuery .= "$sortOption";
+        } else {
+            $userQuery .= "users.userID";
+        }
+
+        if (isset($_GET['orderBy'])) {
+            $orderOption = $_GET['orderBy'];
+            $userQuery .= " $orderOption;";
+        }
 
         $this->userResult = executeQuery($userQuery);
     }
 
-    public function queryUserInfo($userID) {
+    public function queryUserInfo($userID)
+    {
         $userQuery = "SELECT users.*, logins.loginDate AS recentLogin FROM users
         LEFT JOIN logins ON users.userID = logins.userID
         WHERE users.userID = '$userID' AND users.role = 'user' AND logins.loginDate = (
@@ -666,7 +694,8 @@ class User {
         $this->userResult = executeQuery($userQuery);
     }
 
-    public function setAttributes($userRow) {
+    public function setAttributes($userRow)
+    {
         $this->userID = $userRow['userID'];
         $this->firstName = $userRow['firstName'];
         $this->lastName = $userRow['lastName'];
@@ -678,53 +707,57 @@ class User {
         $this->status = (strtotime($this->recentLogin) > strtotime('-30 days')) ? "Active" : "Inactive";
     }
 
-    public function createRow($userRow) {
+    public function createRow($userRow)
+    {
         $this->setAttributes($userRow);
+        $willDisable = ($this->status == "Active") ? "disabled" : "";
 
         return '
         <tr>
-            <td>'.$this->userID.'</td>
-            <td>'.$this->firstName.'</td>
-            <td>'.$this->lastName.'</td>
-            <td>'.$this->username.'</td>
-            <td>'.date('Y-m-d', strtotime($this->recentLogin)).'</td>
+            <td>' . $this->userID . '</td>
+            <td>' . $this->firstName . '</td>
+            <td>' . $this->lastName . '</td>
+            <td>' . $this->username . '</td>
+            <td>' . date('Y-m-d', strtotime($this->recentLogin)) . '</td>
 
             <!-- Action Buttons -->
             <td class="text-center">
                 <!-- View -->
-                <a class="btn btn-primary my-1" href="view.php?id='.$this->userID.'">
+                <a class="btn btn-primary my-1" href="view.php?id=' . $this->userID . '">
                     View
                 </a>
 
                 <!-- Delete -->
-                <a class="btn btn-danger my-1" data-bs-toggle="modal"
-                    data-bs-target="#deleteUser'.$this->userID.'">
+                <a class="btn btn-danger my-1 '. $willDisable .'" data-bs-toggle="modal"
+                    data-bs-target="#deleteUser'. $this->userID . 'Modal">
                     Delete
                 </a>
             </td>
         </tr>
+        '. loadDeleteUserModal($this->userID, $this->firstName, $this->lastName) . '
         ';
     }
 
-    public function createCard() {
+    public function createCard()
+    {
         return '
         <div class="card stat-card w-100">
             <div class="row py-2">
                 <!-- Profile Picture -->
                 <div class="col-12 col-md-2 p-2 d-flex justify-content-center align-items-center">
-                    <img src="../assets/images/userProfile/'.$this->profilePicture.'" alt="Profile Picture"
+                    <img src="../assets/images/userProfile/' . $this->profilePicture . '" alt="Profile Picture"
                         class="rounded-circle img-fluid" width="150" height="140">
                 </div>
 
                 <!-- User Information -->
                 <div class="col-12 col-md-10 pt-3 d-flex justify-content-start align-items-center">
                     <ul class="list-unstyled">
-                        <li><b>'.$this->firstName." " .$this->lastName.'</b></li>
-                        <li><i>'.$this->username.'</i></li>
+                        <li><b>' . $this->firstName . " " . $this->lastName . '</b></li>
+                        <li><i>' . $this->username . '</i></li>
                         <br>
-                        <li><b>Birthday:</b> '.$this->birthday.'</li>
-                        <li><b>Email:</b> '.$this->email.'</li>
-                        <li><b>Status:</b> '.$this->status.'</li>
+                        <li><b>Birthday:</b> ' . $this->birthday . '</li>
+                        <li><b>Email:</b> ' . $this->email . '</li>
+                        <li><b>Status:</b> ' . $this->status . '</li>
                     </ul>
                 </div>
             </div>
@@ -732,7 +765,8 @@ class User {
         ';
     }
 
-    public function showRecentLogins($userID) {
+    public function showRecentLogins($userID)
+    {
         $loginsQuery = "SELECT * FROM `logins` WHERE userID = '$userID' ORDER BY loginDate DESC";
         $loginsResult = executeQuery($loginsQuery);
 
@@ -743,12 +777,12 @@ class User {
             while ($loginRow = mysqli_fetch_assoc($loginsResult)) {
                 $loginDate = date('F j, Y', strtotime($loginRow['loginDate']));
                 $loginTime = date('h:i A', strtotime($loginRow['loginDate']));
-                
+
                 $loginRows .= '
                 <tr class="text-center align-middle">
-                    <td scope="row">'.$loginNo.'</td>
-                    <td>'.$loginDate.'</td>
-                    <td>'.$loginTime.'</td>
+                    <td scope="row">' . $loginNo . '</td>
+                    <td>' . $loginDate . '</td>
+                    <td>' . $loginTime . '</td>
                 </tr>
                 ';
                 $loginNo++;
