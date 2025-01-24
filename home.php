20f81587a81e4ee2a2510e2c96c1dfeb995b2c5a
Md-Lei <dharenlei7@gmail.com>
2025-01-24 11:07:57 +0800

    [BE] - Modularized Add Categories

--- home.php
+++ home.php
@@ -105,27 +105,8 @@ $transactionsResult = executeQuery($transactionsQuery);
 
 
 // Add new categories
-$_SESSION['userID'] = $userID;
-if (isset($_POST['btnSaveCategory'])) {
-    $categoryType = $_POST['categoryType'];
-    $categoryName = $_POST['categoryName'];
+include ("assets\php\addCategories.php");
 
-    if (!empty($categoryType) && !empty($categoryName)) {
-        $categoryQuery = "INSERT INTO categories (userID, categoryType, categoryName) VALUES ('$userID', '$categoryType', '$categoryName')";
-        executeQuery($categoryQuery);
-        // header("Location:home.php");
-        $_SESSION['category_added'] = true;
-    } else {
-        echo '<script>alert("Please fill both category type and category name.")</script>';
-    }
-}
-
-if (isset($_POST['btnSuccessModal'])) {
-    unset($_SESSION['category_added']);
-    header("Location:home.php");
-
-    exit();
-}
 
 
 // Delete Categories
