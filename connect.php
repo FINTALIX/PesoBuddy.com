0ee37e52c88fe65643732e0e4d4d99f41a0e3e44
AJBernardo <austinebernardo8104@gmail.com>
2025-01-25 13:46:24 +0800

    404

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
   $dbhost = "localhost";
 	$dbuser = "root";
 	$dbpass = "";
-	$db = "pesobuddy";
+	$db = "finals";
 
 	$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
 
